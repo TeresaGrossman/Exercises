8f9554433ef8100473d56a40d0124d60fbebf4ed
TeresaG <tmg.coder@gmail.com>
2019-04-21 09:32:59 -0700

    reverse with for loop

--- ReverseString/index.js
+++ ReverseString/index.js
@@ -7,17 +7,35 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-//split() method to return a new array 'arr'
-     var arr = str.split("");
-//reverse() method to reverse the letters in the string
-     arr.reverse("");
-//join() method to rejoin the letters in reverse
-     arr.join("");
-//return the reversed string
-     return arr.join("");
+//FOR LOOP SOLUTION-
+// Make an empty string. 
+// Take each character from the original string and stick it into the new one, one at a TimeRanges.
+// Because adding one character at a time, the string is reversed.
+let reversed = '';
 
-// cleaned up code return str.split("").reverse().join("");
+// same as below for (var i = 0; i < str.length; i++){ use this one if more than one 
+// }
+for (let character of str){
+    reversed = character + reversed;
 }
+return reversed;
+}
+
+// //REVERSE HELPER
+// //split() method to return a new ARRAY 'arr'
+// var arr = str.split("");
+// //reverse() method to reverse the letters in the ARRAY
+//      arr.reverse("");
+// //join() method to rejoin the letters in reverse
+//      arr.join("");
+// //return the reversed string
+//      return arr.join("");
+
+// // cleaned up code 
+//     //return str.split("")
+//     //.reverse().
+//     //join("");
+
 
 module.exports = reverse;
 // 
