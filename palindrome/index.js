2f35400ba8a50af0e67e467f7bc2233b785929b5
TeresaG <tmg.coder@gmail.com>
2019-06-23 13:10:53 -0700

    advanced array helper "EVERY" function

--- palindrome/index.js
+++ palindrome/index.js
@@ -7,13 +7,23 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
+//Easy solution
 function palindrome(str) {
-  const reversed = str
-    .split("")
-    .reverse()
-    .join("");
-
-  return str === reversed;
+  return str.split("").every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
 }
 
 module.exports = palindrome;
+
+//Easy solution
+// function palindrome(str) {
+//     const reversed = str
+//       .split("")
+//       .reverse()
+//       .join("");
+
+//     return str === reversed;
+//   }
+
+//   module.exports = palindrome;
