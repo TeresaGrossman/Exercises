b5efcf2df4628518493016c33047aa106d83b1ba
TeresaG <tmg.coder@gmail.com>
2019-06-25 10:17:13 -0700

    solution working

--- reverseInteger/index.js
+++ reverseInteger/index.js
@@ -8,6 +8,24 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+//Helper convert number to a string .toString()
+//Helper convert string to a number/integer .parseInt() - Needs to be at front with everything in ()
+//Helper ensure sign is the same - Math.sign(x)
+
+function reverseInt(n) {
+    //Convert integer to a string and then reverse
+  const reversed = n
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+//Make sure the 'negative' is passed through if needed
+//or use math.sign instead of if statement
+if (n < 0) {
+    return parseInt(reversed) * -1;
+}
+//Turn string back into an integer
+    return parseInt(reversed);
+}
 
 module.exports = reverseInt;
