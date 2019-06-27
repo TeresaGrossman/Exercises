dc2f4b6cbd0338b2b65381fb71ad59ba67c4dcc4
TeresaG <tmg.coder@gmail.com>
2019-06-27 12:08:58 -0700

    solved - keep is simple

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -11,7 +11,26 @@
 //   fizz
 //   4
 //   buzz
+//use module operator '%' tells you if there is a remainder
 
-function fizzBuzz(n) {}
+
+function fizzBuzz(n) {
+    
+    for (let i = 1; i <= n; i++) {
+        //check multiple of 3 & 5
+        if (i % 3 === 0 && i % 5){  //can also be if (i % 15 )
+        console.log('fizzBuzz');
+    } else if(i % 3 === 0){
+        //is the number a multiple of 3?
+        console.log('fizz');
+    } else if(i % 5 === 0){
+        //is the number a multiple of 5?
+        console.log('buzz');
+    } else{
+        console.log(i);
+    }
+}
+
+}
 
 module.exports = fizzBuzz;
