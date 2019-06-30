0af58f66162cf461fe073d045d6ed89728bf1952
TeresaG <tmg.coder@gmail.com>
2019-06-30 12:00:36 -0700

    add

--- chunk/index.js
+++ chunk/index.js
@@ -12,7 +12,7 @@ function chunk(array, size) {
 const chunked = [];
 
 for (let element of array){
-    const last = chunked[chunked.length -1];
+    const last = chunked[chunked.length - 1];
 
     if (!last || last.length === size){
         chunked.push([element]);
