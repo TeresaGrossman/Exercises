3d400897d14601d34488a5cf7ab86636df933ae9
TeresaG <tmg.coder@gmail.com>
2019-06-30 12:28:51 -0700

    slice method

--- chunk/index.js
+++ chunk/index.js
@@ -8,19 +8,35 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+//SLICE METHOD letters.slice(0, 3); makes a copy of each letter from ZERO to TWO, NOT the third one
 function chunk(array, size) {
-const chunked = [];
-
-for (let element of array){
-    const last = chunked[chunked.length - 1];
-
-    if (!last || last.length === size){
-        chunked.push([element]);
-    } else{
-        last.push(element);
-    }
-}
-return chunked;
+ const chunked = [];
+ let index = 0;
+ //use a while loop to run as long as less than array length
+ while (index < array.length){
+     chunked.push(array.slice(index, index + size)); 
+     index += size;
+     //array.slice produces an array that contains some number of
+     //elements(index, index + size ) from the original array and puts them into the 'chunked' array
+     //above using the push method.
+ }
+ return chunked;
 }
 
 module.exports = chunk;
+
+// First soln
+//  function chunk(array, size) {
+//     const chunked = [];
+    
+//     for (let element of array){
+//         const last = chunked[chunked.length - 1];
+    
+//         if (!last || last.length === size){
+//             chunked.push([element]);
+//         } else{
+//             last.push(element);
+//         }
+//     }
+//     return chunked;
+//     }
